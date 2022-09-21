fb6609e170902cea72fa526ff60962d8e46f474f
Steve Kindstrand <stevekindstrand@gmail.com>
2022-09-21 10:51:08 +0200

    open menu on click

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const menu = document.getElementById("menu");
+const menuLinks = document.getElementById("menuLinks");
 
 menu.onclick = function () {
   menu.classList.toggle("openMenu");
+  menuLinks.classList.toggle("openMenuLinks");
 };
