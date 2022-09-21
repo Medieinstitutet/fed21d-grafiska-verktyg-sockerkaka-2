640f10b89fa41fe5ee298b84aed8eea28d82f903
Steve Kindstrand <stevekindstrand@gmail.com>
2022-09-21 15:10:50 +0200

    mobile & tablet done

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ const menuLinks = document.getElementById("menuLinks");
 menu.onclick = function () {
   menu.classList.toggle("openMenu");
   menuLinks.classList.toggle("openMenuLinks");
+  document.body.style.overflowY =
+    document.body.style.overflowY === "hidden" ? "visible" : "hidden";
 };
