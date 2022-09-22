0f7bfc5a603e49a946d8147ad82af698843985ef
Steve Kindstrand <stevekindstrand@gmail.com>
2022-09-22 11:51:25 +0200

    almost done, animtions on icon letters left

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+// Menu
 const menu = document.getElementById("menu");
 const menuLinks = document.getElementById("menuLinks");
 
-menu.onclick = function () {
+menu.addEventListener("click", () => {
   menu.classList.toggle("openMenu");
   menuLinks.classList.toggle("openMenuLinks");
   document.body.style.overflowY =
     document.body.style.overflowY === "hidden" ? "visible" : "hidden";
-};
+});
+
+// Cookies
+document.getElementById("cookieBtn").addEventListener("click", () => {
+  document.getElementById("cookieContainer").style.display = "none";
+});
